db650ba064e205fd237450dd5077df3e032f5c8f
unknown <lndubourg@gmail.com>
2023-09-07 12:45:01 +0200

    finalisation du hamburger de la navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
     const links = [
         {
             id: 1,
@@ -19,7 +20,7 @@ const Navbar = () => {
         }
     ]
   return (
-        <div className="flex justify-between items-center w-full h-20 fixed bg-darkblue px-4">
+        <div className="flex justify-between items-center w-full h-20 fixed bg-gradient-to-b from-lightblue to-darkblue">
             <h1 className='text-3xl text-center font-title'>Voilier Nrj</h1>
             
             {/* hidden pour cacher suivant la largeur de l'écran */}
@@ -34,9 +35,20 @@ const Navbar = () => {
                 ))}
             </ul>
             {/* menu hamburger */}
-            <div className='cursor-pointer pr-4 z-10 text-black'>
-                <FaBars size={30}/>
+            <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-black'>
+                {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
             </div>
+            {nav && (
+                <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-lightblue to-darkblue'>
+                    {links.map(({title, id}) => (
+                            <li 
+                                key={id}
+                                className='px-4 cursor-pointer capitalize py-6 text-4xl'>
+                                    {title}
+                            </li>))}
+            </ul>
+            )}
+            
         </div>
   )
 }
