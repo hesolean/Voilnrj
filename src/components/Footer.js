e424c77f965263a4e7d0b052b79cfaac723ba61e
unknown <lndubourg@gmail.com>
2023-09-08 17:23:39 +0200

    form sur le footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 
 const Footer = () => {
+	const [message, setMessage] = useState('')
     const [inputValue, setInputValue] = useState('')
 
+	function handleSubmit(e) {
+		e.preventDefault()
+		alert(`Merci d'avoir partage ${message} avec moi. Je vous répondrai à l'adresse ${inputValue}.`)
+	}
 	function handleInput(e) {
 		setInputValue(e.target.value)
 	}
@@ -19,17 +24,28 @@ const Footer = () => {
   return (
     <div name='footer'>
         <footer className='text-black p-4 border-t-black border-2 flex justify-start flex-col items-center text-xl bg-lightblue'>
-			<div className='mb-2'>
-				Quand vous aimez brasser de l'air ...
-			</div>
-			<div className='mb-2'>Laissez-moi votre mail :</div>
-			<input
-                className='p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none'
-				placeholder='Entrez votre mail'
-				onChange={handleInput}
-				value={inputValue}
-				onBlur={handleBlur}
-			/>
+			<form onSubmit={handleSubmit} className='flex flex-col justify-center m-8'>
+				<h3 className='mb-2'>Partagez avec moi sur notre passion commune : <u>brasser du vent !</u></h3>
+				<textarea
+					className='my-8 p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none'
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
+					placeholder="Dites m'en plus sur votre passion."
+				></textarea>
+
+				<input
+					className='p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none'
+					placeholder='Entrez votre mail'
+					onChange={handleInput}
+					value={inputValue}
+					onBlur={handleBlur}
+				/>
+				<button
+					type='submit'
+					className='mt-8'
+				>Partager</button>
+			</form>
+			
 		</footer>
     </div>
   )
