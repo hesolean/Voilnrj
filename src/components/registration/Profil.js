59a59185b64733ead63b5bee766b3d9c565f666d
unknown <lndubourg@gmail.com>
2023-09-14 15:08:37 +0200

    mise en page du profil

--- src/components/registration/Profil.js
+++ src/components/registration/Profil.js
@@ -17,41 +17,42 @@ const initialValues = {
 
 const savedValues = {
   profilName: "Jackson",
-    foreName: "Mickael",
-    adress: {
-      number: "222",
-      street: "backer Street",
-      zipCode: "22222",
-      town: "Tombouctou",
-    },
-    phoneNumbers: ["0123456789"],
-  };
+  foreName: "Mickael",
+  adress: {
+    number: "222",
+    street: "backer Street",
+    zipCode: "22222",
+    town: "Tombouctou",
+  },
+  phoneNumbers: ["0123456789"],
+};
 const onSubmit = async (values, onSubmitProps) => {
-    console.log('first')
-    try {
-      console.log('Form data : ', values);
-      console.log('submit props : ', onSubmitProps);
-    onSubmitProps.setSubmitting(false)
-    onSubmitProps.resetForm()
-}catch (error) {
-    console.error('Erreur lors de la soumission : ', error);
+  console.log("first");
+  try {
+    console.log("Form data : ", values);
+    console.log("submit props : ", onSubmitProps);
+    onSubmitProps.setSubmitting(false);
+    onSubmitProps.resetForm();
+  } catch (error) {
+    console.error("Erreur lors de la soumission : ", error);
   }
 };
 const validationSchema = Yup.object({
-    name: Yup.string().required("Requis"),
-    foreName: Yup.string().required("Requis"),
-    // adress: {
-    //   number: Yup.number().required("Requis"),
-    //   street: Yup.string().required("Requis"),
-    //   zipCode: Yup.number().required("Requis"),
-    //   town: Yup.string().required("Requis"),
-    // },
-    phoneNumbers: Yup.array().of(Yup.string().matches(/^\d{10}$/)).required("Requis"),
-  });
-  
+  name: Yup.string().required("Requis"),
+  foreName: Yup.string().required("Requis"),
+  // adress: {
+  //   number: Yup.number().required("Requis"),
+  //   street: Yup.string().required("Requis"),
+  //   zipCode: Yup.number().required("Requis"),
+  //   town: Yup.string().required("Requis"),
+  // },
+  phoneNumbers: Yup.array()
+    .of(Yup.string().matches(/^\d{10}$/))
+    .required("Requis"),
+});
 
 function Profil() {
-    const [formValues, setFormValues] = useState(null)
+  const [formValues, setFormValues] = useState(null);
   return (
     <div
       name="profil"
@@ -64,7 +65,7 @@ function Profil() {
           </p>
         </div>
         <Formik
-        //il test formValues. Si absent, il prend initialValues. L'inverse ne fonctionne pas
+          //il test formValues. Si absent, il prend initialValues. L'inverse ne fonctionne pas
           initialValues={formValues || initialValues}
           validationSchema={validationSchema}
           onSubmit={onSubmit}
@@ -75,70 +76,80 @@ function Profil() {
           validateOnBlur={false}
         >
           {/* on donne accès à toutes les méthodes qui sont liées au formulaire */}
-          {formik => {
-            console.log('formik : ',formik)
+          {(formik) => {
+            console.log("formik : ", formik);
             return (
               <div className="flex justify-center items-center">
                 <Form className="flex flex-col w-full md:w-1/2">
-                  <label htmlFor="identifiant" className="text-2xl">
-                    Nom
-                  </label>
-                  <Field
-                    type="text"
-                    id="profilName"
-                    name="profilName"
-                    placeholder="Entrez votre nom"
-                    className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none"
-                  />
-                  <ErrorMessage name="name" component={TextError} />
+                  <div className="flex flex-col  justify-center items-center md: flex-row">
+                    <label htmlFor="identifiant" className="text-2xl mr-3">
+                      Nom
+                    </label>
+                    <Field
+                      type="text"
+                      id="profilName"
+                      name="profilName"
+                      placeholder="Entrez votre nom"
+                      className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none"
+                    />
+                    <ErrorMessage name="name" component={TextError} />
 
-                  <label htmlFor="foreName" className="text-2xl mb-3 mt-2 mr-3">
-                    Prénom
-                  </label>
-                  <Field
-                    type="text"
-                    id="foreName"
-                    name="foreName"
-                    placeholder="Entrez votre prénom"
-                    className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none"
-                  />
-                  <ErrorMessage name="foreName" component={TextError} />
+                    <label
+                      htmlFor="foreName"
+                      className="text-2xl my-3 mr-3 ml-6"
+                    >
+                      Prénom
+                    </label>
+                    <Field
+                      type="text"
+                      id="foreName"
+                      name="foreName"
+                      placeholder="Entrez votre prénom"
+                      className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none"
+                    />
+                    <ErrorMessage name="foreName" component={TextError} />
+                  </div>
+                  <div className="mt-10">
+                    <label htmlFor="adress" className="text-2xl mb-3 mt-8 mr-3">
+                      Adresse
+                    </label>
+                    <div className="flex flex-wrap">
+                      <Field
+                        type="text"
+                        id="number"
+                        name="adress.number"
+                        placeholder="Numero"
+                        className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none w-24"
+                      />
+                      <Field
+                        type="text"
+                        id="street"
+                        name="adress.street"
+                        placeholder="Entrez le nom de la voie"
+                        className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none w-96"
+                      />
+                      <ErrorMessage name="street" component={TextError} />
+                      <Field
+                        type="text"
+                        id="zipCode"
+                        name="adress.zipCode"
+                        placeholder="Code postal"
+                        className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none w-24"
+                      />
+                      <ErrorMessage name="number" component={TextError} />
+                      <Field
+                        type="text"
+                        id="town"
+                        name="adress.town"
+                        placeholder="Entrez la ville"
+                        className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none"
+                      />
+                      <ErrorMessage name="town" component={TextError} />
+                    </div>
+                  </div>
 
-                  <label htmlFor="adress" className="text-2xl mb-3 mt-2 mr-3">
-                    Adresse
-                  </label>
-                  <Field
-                    type="number"
-                    id="number"
-                    name="adress.number"
-                    className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none"
-                  />
-                  <Field
-                    type="text"
-                    id="street"
-                    name="adress.street"
-                    placeholder="Entrez le nom de la voie"
-                    className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none"
-                  />
-                  <ErrorMessage name="street" component={TextError} />
-                  <Field
-                    type="number"
-                    id="zipCode"
-                    name="adress.zipCode"
-                    className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none"
-                  />
-                  <ErrorMessage name="number" component={TextError} />
-                  <Field
-                    type="text"
-                    id="town"
-                    name="adress.town"
-                    placeholder="Entrez la ville"
-                    className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none"
-                  />
-                  <ErrorMessage name="town" component={TextError} />
-
-                  <div>
-                    <label htmlFor="phone" className="text-2xl mb-3 mt-2 mr-3">
+                  <div className="mt-10">
+                    <label htmlFor="phone" className="text-2xl mb-3 mt-8 mr-3">
                       Téléphone
                     </label>
                     <FieldArray
@@ -182,7 +193,8 @@ function Profil() {
                       }}
                     </FieldArray>
                   </div>
-                  <button
+                  <div className="flex flex-row justify-between w-full">
+                    <button
                     type="button"
                     onClick={() => setFormValues(savedValues)}
                     className="group w-fit px-6 py-3 my-2 flex items-center rounded-md text-lightblue bg-gradient-to-r from-blue to-black cursor-pointer hover:scale-110 duration-300"
@@ -197,11 +209,13 @@ function Profil() {
                   </button>
                   <button
                     type="submit"
-                    disabled={!(formik.isValid) || formik.isSubmitting}
+                    disabled={!formik.isValid || formik.isSubmitting}
                     className="group w-fit px-6 py-3 my-2 flex items-center rounded-md text-lightblue bg-gradient-to-r from-blue to-black cursor-pointer hover:scale-110 duration-300"
                   >
                     Valider
                   </button>
+                  </div>
+                  
                 </Form>
               </div>
             );
