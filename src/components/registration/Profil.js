eb86196b227e33758b484fa51b11b06f853b19cc
unknown <lndubourg@gmail.com>
2023-09-14 16:22:35 +0200

    ajustement des champs par rapport au back

--- src/components/registration/Profil.js
+++ src/components/registration/Profil.js
@@ -6,25 +6,17 @@ import TextError from "../formik/TextError";
 const initialValues = {
   profilName: "",
   foreName: "",
-  adress: {
-    number: "",
-    street: "",
-    zipCode: "",
-    town: "",
-  },
+  profilEmail: "",
   phoneNumbers: [""],
+  profilPassword: "",
 };
 
 const savedValues = {
   profilName: "Jackson",
   foreName: "Mickael",
-  adress: {
-    number: "222",
-    street: "backer Street",
-    zipCode: "22222",
-    town: "Tombouctou",
-  },
+  profilEmail: "michael.jackson@hollywoood.com",
   phoneNumbers: ["0123456789"],
+  profilPassword: "KingOfPop-01",
 };
 const onSubmit = async (values, onSubmitProps) => {
   console.log("first");
@@ -38,17 +30,13 @@ const onSubmit = async (values, onSubmitProps) => {
   }
 };
 const validationSchema = Yup.object({
-  name: Yup.string().required("Requis"),
+  profilName: Yup.string().required("Requis"),
   foreName: Yup.string().required("Requis"),
-  // adress: {
-  //   number: Yup.number().required("Requis"),
-  //   street: Yup.string().required("Requis"),
-  //   zipCode: Yup.number().required("Requis"),
-  //   town: Yup.string().required("Requis"),
-  // },
+  profilEmail: Yup.string().email("Format email invalide").required("Requis"),
   phoneNumbers: Yup.array()
     .of(Yup.string().matches(/^\d{10}$/))
     .required("Requis"),
+  profilPassword: Yup.string().required("Requis"),
 });
 
 function Profil() {
@@ -81,72 +69,44 @@ function Profil() {
             return (
               <div className="flex justify-center items-center">
                 <Form className="flex flex-col w-full md:w-1/2">
-                  <div className="flex flex-col  justify-center items-center md: flex-row">
-                    <label htmlFor="identifiant" className="text-2xl mr-3">
-                      Nom
-                    </label>
-                    <Field
-                      type="text"
-                      id="profilName"
-                      name="profilName"
-                      placeholder="Entrez votre nom"
-                      className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none"
-                    />
-                    <ErrorMessage name="name" component={TextError} />
+                  <label htmlFor="identifiant" className="text-2xl mr-3">
+                    Nom
+                  </label>
+                  <Field
+                    type="text"
+                    id="profilName"
+                    name="profilName"
+                    placeholder="Entrez votre nom"
+                    className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none"
+                  />
+                  <ErrorMessage name="name" component={TextError} />
 
-                    <label
-                      htmlFor="foreName"
-                      className="text-2xl my-3 mr-3 ml-6"
-                    >
-                      Prénom
-                    </label>
-                    <Field
-                      type="text"
-                      id="foreName"
-                      name="foreName"
-                      placeholder="Entrez votre prénom"
-                      className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none"
-                    />
-                    <ErrorMessage name="foreName" component={TextError} />
-                  </div>
-                  <div className="mt-10">
-                    <label htmlFor="adress" className="text-2xl mb-3 mt-8 mr-3">
-                      Adresse
-                    </label>
-                    <div className="flex flex-wrap">
-                      <Field
-                        type="text"
-                        id="number"
-                        name="adress.number"
-                        placeholder="Numero"
-                        className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none w-24"
-                      />
-                      <Field
-                        type="text"
-                        id="street"
-                        name="adress.street"
-                        placeholder="Entrez le nom de la voie"
-                        className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none w-96"
-                      />
-                      <ErrorMessage name="street" component={TextError} />
-                      <Field
-                        type="text"
-                        id="zipCode"
-                        name="adress.zipCode"
-                        placeholder="Code postal"
-                        className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none w-24"
-                      />
-                      <ErrorMessage name="number" component={TextError} />
-                      <Field
-                        type="text"
-                        id="town"
-                        name="adress.town"
-                        placeholder="Entrez la ville"
-                        className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none"
-                      />
-                      <ErrorMessage name="town" component={TextError} />
-                    </div>
-                  </div>
+                  <label htmlFor="foreName" className="text-2xl my-3 mr-3 ml-6">
+                    Prénom
+                  </label>
+                  <Field
+                    type="text"
+                    id="foreName"
+                    name="foreName"
+                    placeholder="Entrez votre prénom"
+                    className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none"
+                  />
+                  <ErrorMessage name="foreName" component={TextError} />
+
+                  <label
+                    htmlFor="profilEmail"
+                    className="text-2xl my-3 mr-3 ml-6"
+                  >
+                    Email
+                  </label>
+                  <Field
+                    type="text"
+                    id="profilEmail"
+                    name="profilEmail"
+                    placeholder="Entrez votre email"
+                    className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none"
+                  />
+                  <ErrorMessage name="profilEmail" component={TextError} />
 
                   <div className="mt-10">
                     <label htmlFor="phone" className="text-2xl mb-3 mt-8 mr-3">
@@ -193,29 +153,44 @@ function Profil() {
                       }}
                     </FieldArray>
                   </div>
+
+                  <label
+                    htmlFor="profilPassword"
+                    className="text-2xl my-3 mr-3 ml-6"
+                  >
+                    Mot de passe
+                  </label>
+                  <Field
+                    type="text"
+                    id="profilPassword"
+                    name="profilPassword"
+                    placeholder="Entrez votre mot de passe"
+                    className="p-2 bg-transparent border-2 rounded-md text-blue focus:outline-none"
+                  />
+                  <ErrorMessage name="profilPassword" component={TextError} />
+
                   <div className="flex flex-row justify-between w-full">
                     <button
-                    type="button"
-                    onClick={() => setFormValues(savedValues)}
-                    className="group w-fit px-6 py-3 my-2 flex items-center rounded-md text-lightblue bg-gradient-to-r from-blue to-black cursor-pointer hover:scale-110 duration-300"
-                  >
-                    Recharger le formulaire
-                  </button>
-                  <button
-                    type="reset"
-                    className="group w-fit px-6 py-3 my-2 flex items-center rounded-md text-lightblue bg-gradient-to-r from-blue to-black cursor-pointer hover:scale-110 duration-300"
-                  >
-                    Réinitialiser
-                  </button>
-                  <button
-                    type="submit"
-                    disabled={!formik.isValid || formik.isSubmitting}
-                    className="group w-fit px-6 py-3 my-2 flex items-center rounded-md text-lightblue bg-gradient-to-r from-blue to-black cursor-pointer hover:scale-110 duration-300"
-                  >
-                    Valider
-                  </button>
+                      type="button"
+                      onClick={() => setFormValues(savedValues)}
+                      className="group w-fit px-6 py-3 my-2 flex items-center rounded-md text-lightblue bg-gradient-to-r from-blue to-black cursor-pointer hover:scale-110 duration-300"
+                    >
+                      Recharger le formulaire
+                    </button>
+                    <button
+                      type="reset"
+                      className="group w-fit px-6 py-3 my-2 flex items-center rounded-md text-lightblue bg-gradient-to-r from-blue to-black cursor-pointer hover:scale-110 duration-300"
+                    >
+                      Réinitialiser
+                    </button>
+                    <button
+                      type="submit"
+                      disabled={!formik.isValid || formik.isSubmitting}
+                      className="group w-fit px-6 py-3 my-2 flex items-center rounded-md text-lightblue bg-gradient-to-r from-blue to-black cursor-pointer hover:scale-110 duration-300"
+                    >
+                      Valider
+                    </button>
                   </div>
-                  
                 </Form>
               </div>
             );
