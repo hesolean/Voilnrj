bb276229e315f3cb39d4c0c7421cc66480a73c6f
unknown <lndubourg@gmail.com>
2023-09-20 16:28:25 +0200

    affichage des champs quand le serveur a renvoyé les données

--- src/components/wind/UpdateWind.js
+++ src/components/wind/UpdateWind.js
@@ -1,21 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import { Formik, Form } from "formik";
-import axios from 'axios'
+import axios from "axios";
 
 import FormikControl from "../formElements/FormikControl";
+import Loader from "../Loader";
 
 import { categoryList } from "../../datas/categoryList";
 import { coverList } from "../../datas/coverList";
 
 import { newWindValidationSchema } from "../services/ValidationSchemas";
 
-
-
 const handleSubmit = async (values, onSubmitProps) => {
   try {
     console.log("Form data : ", values);
     console.log("submit props : ", onSubmitProps);
-    axios.post(`http://localhost:8080/winds`, values).then((res) => {
+    axios.put(`http://localhost:8080/winds`, values).then((res) => {
       console.log(res);
     });
     onSubmitProps.setSubmitting(false);
@@ -24,75 +23,96 @@ const handleSubmit = async (values, onSubmitProps) => {
     console.error("Erreur lors de la soumission : ", error);
   }
 };
-function UpdateWind(updatedWind) {
 
+function UpdateWind(id) {
+  const [updatedWind, setUpdatedWind] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    // console.log(id)
+    // console.log("url",`http://localhost:8080/winds/${id.id}` )
+    axios
+      .get(`http://localhost:8080/winds/${id.id}`)
+      .then((res) => {
+        setUpdatedWind(res.data);
+        console.log("updatedWind ", updatedWind);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
   return (
-
-    <div
-      name="newWind"
-      className="w-full bg-gradient-to-b from-darkblue to-lightblue text-dark md:h-screen"
-    >
+    <div name="newWind" className=" bg-darkblue text-dark">
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8">
           <p className="text-4xl front-bold inline border-b-4 border-black">
             Nouveau produit
           </p>
         </div>
-        <Formik
-          initialValues={updatedWind}
-          // validationSchema={newWindValidationSchema}
-          onSubmit={handleSubmit}
-        >
-          {(formik) => (
-            <div className="flex justify-center items-center">
-              <Form className="flex flex-col w-full md:w-1/2">
-                <FormikControl
-                  control="input"
-                  type="text"
-                  label="Nom du produit"
-                  name="windName"
-                />
+        <div>
+          {loading ? (
+            <Loader />
+          ) : (
+            <Formik
+              initialValues={updatedWind}
+              // validationSchema={newWindValidationSchema}
+              onSubmit={handleSubmit}
+            >
+              {(formik) => (
+                <div className="flex justify-center items-center">
+                  <Form className="flex flex-col w-full md:w-1/2">
+                    <FormikControl
+                      control="input"
+                      type="text"
+                      label="Nom"
+                      name="windName"
+                    />
 
-                <FormikControl
-                  control="textarea"
-                  label="Description"
-                  name="windDescription"
-                />
+                    <FormikControl
+                      control="textarea"
+                      label="Description"
+                      name="windDescription"
+                    />
 
-                <FormikControl
-                  control="select"
-                  label="Catégorie"
-                  name="windCategory"
-                  options={categoryList}
-                />
+                    <FormikControl
+                      control="select"
+                      label="Catégorie"
+                      name="windCategory"
+                      options={categoryList}
+                    />
 
-                <FormikControl
-                  control="input"
-                  type="number"
-                  label="Prix"
-                  name="windPrice"
-                />
-                
-                <FormikControl
-                  control="select"
-                  label="Image"
-                  name="windCover"
-                  options={coverList}
-                />
+                    <FormikControl
+                      control="input"
+                      type="number"
+                      label="Prix"
+                      name="windPrice"
+                    />
 
-                <button
-                  type="submit"
-                  className="group w-fit px-6 py-3 my-2 flex items-center rounded-md text-lightblue bg-gradient-to-r from-blue to-black cursor-pointer hover:scale-110 duration-300"
-                >
-                  Valider
-                </button>
-              </Form>
-            </div>
+                    <FormikControl
+                      control="select"
+                      label="Image"
+                      name="windCover"
+                      options={coverList}
+                    />
+
+                    <button
+                      type="submit"
+                      className="group w-fit px-6 py-3 my-2 flex items-center rounded-md text-lightblue bg-gradient-to-r from-blue to-black cursor-pointer hover:scale-110 duration-300"
+                    >
+                      Valider
+                    </button>
+                  </Form>
+                </div>
+              )}
+            </Formik>
           )}
-        </Formik>
+        </div>
       </div>
     </div>
   );
 }
 
-export default UpdateWind
+export default UpdateWind;
--- src/components/wind/WindProducts.js
+++ src/components/wind/WindProducts.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { Disclosure } from "@headlessui/react";
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
 
 import axios from "axios";
 
@@ -14,7 +16,6 @@ const WindProducts = ({ cart, updateCart }) => {
     console.log("selectedCat", selectedCategory);
   };
 
-  const [updatedWind, setUpdatedWind] = useState("");
 
   const [winds, setWinds] = useState([]);
 
@@ -48,19 +49,6 @@ const WindProducts = ({ cart, updateCart }) => {
       updateCart([...cart, { windName, windPrice, windAmount: 1 }]);
     }
     alert("Ajouté à la voile !");
-  }
-
-  function updateWind(id) {
-    axios
-      .get(`http://localhost:8080/winds/${id}`)
-      .then((res) => {
-        setUpdatedWind(res.data);
-        console.log("updatedWind ", updatedWind);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    <UpdateWind updatedWind={updatedWind} />;
   }
 
   function totalDelete(id) {
@@ -102,28 +90,42 @@ const WindProducts = ({ cart, updateCart }) => {
                   windPrice={windPrice}
                   windDescription={windDescription}
                 />
-                <div className="flex flex-row">
-                  <button
-                    className="group w-fit px-6 py-3 m-2 flex items-center rounded-md text-lightblue bg-gradient-to-r from-blue to-black cursor-pointer hover:scale-110 duration-300"
-                    onClick={() => addToCart(windName, windPrice)}
-                  >
-                    Ajouter à la voile
-                  </button>
 
-                  <button
-                    className="group w-fit px-6 py-3 m-2 flex items-center rounded-md text-lightblue bg-gradient-to-r from-green to-black cursor-pointer hover:scale-110 duration-300"
-                    onClick={() => updateWind(id)}
-                  >
-                    Modifier le vent
-                  </button>
+                <button
+                  className="group w-fit px-6 py-3 m-2 flex items-center rounded-md text-lightblue bg-gradient-to-r from-blue to-black cursor-pointer hover:scale-110 duration-300"
+                  onClick={() => addToCart(windName, windPrice)}
+                >
+                  Ajouter à la voile
+                </button>
 
-                  <button
-                    className="group w-fit px-6 py-3 m-2 flex items-center rounded-md text-lightblue bg-gradient-to-r from-red to-black cursor-pointer hover:scale-110 duration-300"
-                    onClick={() => totalDelete(id)}
-                  >
-                    Supprimer de la gamme
-                  </button>
-                </div>
+                <Disclosure className="flex flex-row">
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button
+                        className="group w-fit px-6 py-3 m-2 flex items-center rounded-md text-lightblue bg-gradient-to-r from-green to-black cursor-pointer hover:scale-110 duration-300"
+                        onClick={() => {
+                          console.log("id", id)
+                        }
+                        }
+                      >
+                        Modifier le vent
+                        <ChevronRightIcon
+                          className={open ? "rotate-90 transform" : ""}
+                        />
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="text-black">
+                        <UpdateWind id={id} />
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+
+                <button
+                  className="group w-fit px-6 py-3 m-2 flex items-center rounded-md text-lightblue bg-gradient-to-r from-red to-black cursor-pointer hover:scale-110 duration-300"
+                  onClick={() => totalDelete(id)}
+                >
+                  Supprimer de la gamme
+                </button>
               </div>
             ) : null
         )}
